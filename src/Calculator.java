37e86304b991fea280cc6f54cfa5ab9b47efedbb
Riccardo Petruzziello <rpetruzziello@outlook.it>
2021-12-01 18:09:41 +0100

    updated calculator class for variable manipulation

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,19 @@
 package unisa.group1.test_scalc;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.property.SimpleStringProperty;
 import org.apache.commons.math.complex.Complex;
 import org.apache.commons.math.complex.ComplexFormat;
 
@@ -18,7 +29,7 @@ public class Calculator {
     
     /**
      * Auxiliary internal class for complex numbers.
-     *
+     * 
      */
     public class OPT_Complex extends Complex implements Serializable {
         
@@ -31,7 +42,7 @@ public class Calculator {
         
         /**
          * Overridden constructor.
-         *
+         * 
          * @param real  the ral part.
          * @param imaginary the imaginary part.
          */
@@ -41,7 +52,7 @@ public class Calculator {
         
         /**
          * Overridden constructor.
-         *
+         * 
          * @param c the basic complex number.
          */
         public OPT_Complex(org.apache.commons.math.complex.Complex c) {
@@ -50,7 +61,7 @@ public class Calculator {
 
         /**
          * Overridden sqrt.
-         *
+         * 
          * @return  the square root.
          */
         @Override
@@ -60,7 +71,7 @@ public class Calculator {
 
         /**
          * Overridden power.
-         *
+         * 
          * @param x the exponent.
          * @return  the power.
          */
@@ -71,7 +82,7 @@ public class Calculator {
 
         /**
          * Overridden subtract.
-         *
+         * 
          * @param rhs   the subtrahend.
          * @return  the difference.
          */
@@ -82,7 +93,7 @@ public class Calculator {
 
         /**
          * Overridden negate.
-         *
+         * 
          * @return  the sign swapped number.
          */
         @Override
@@ -92,7 +103,7 @@ public class Calculator {
 
         /**
          * Overridden multiply.
-         *
+         * 
          * @param rhs   the factor.
          * @return  the product.
          */
@@ -103,7 +114,7 @@ public class Calculator {
 
         /**
          * Overridden divide.
-         *
+         * 
          * @param rhs   the divisor.
          * @return  the quotient.
          */
@@ -114,7 +125,7 @@ public class Calculator {
 
         /**
          * Overridden add.
-         *
+         * 
          * @param rhs   the addend.
          * @return  the sum.
          */
@@ -125,7 +136,7 @@ public class Calculator {
         
         /**
          * Overridden toString with ComplexFormat support.
-         *
+         * 
          * @return  the formatted string.
          */
         @Override
@@ -135,11 +146,129 @@ public class Calculator {
             return "(" + format.format(this) + ")";
         }
         
+        /**
+         * Refactored arg.
+         * 
+         * @return  the phase.
+         */
+        public OPT_Complex arg() {
+            return new OPT_Complex(super.getArgument(), 0.0);
+        }
+
+        /**
+         * Overridden tan.
+         * 
+         * @return  the tangent.
+         */
+        @Override
+        public OPT_Complex tan() {
+            return new OPT_Complex(super.tan());
+        }
+
+        /**
+         * Overridden sin.
+         * 
+         * @return  the sine.
+         */
+        @Override
+        public OPT_Complex sin() {
+            return new OPT_Complex(super.sin());
+        }
+
+        /**
+         * Overridden log.
+         * 
+         * @return  the logarithm.
+         */
+        @Override
+        public OPT_Complex log() {
+            return new OPT_Complex(super.log());
+        }
+
+        /**
+         * Overridden exp.
+         * 
+         * @return  the exponential.
+         */
+        @Override
+        public OPT_Complex exp() {
+            return new OPT_Complex(super.exp());
+        }
+
+        /**
+         * Overridden cos.
+         * 
+         * @return  the cosine.
+         */
+        @Override
+        public OPT_Complex cos() {
+            return new OPT_Complex(super.cos());
+        }
+
+        /**
+         * Overridden atan.
+         * 
+         * @return  the arc tangent.
+         */
+        @Override
+        public OPT_Complex atan() {
+            return new OPT_Complex(super.atan());
+        }
+
+        /**
+         * Overridden asin.
+         * 
+         * @return  the arc sine.
+         */
+        @Override
+        public OPT_Complex asin() {
+            return new OPT_Complex(super.asin());
+        }
+
+        /**
+         * Overridden acos.
+         * 
+         * @return  the arc cosine.
+         */
+        @Override
+        public OPT_Complex acos() {
+            return new OPT_Complex(super.acos());
+        }
+
+        /**
+         * Overridden conjugate.
+         * 
+         * @return  the conjugate.
+         */
+        @Override
+        public OPT_Complex conjugate() {
+            return new OPT_Complex(super.conjugate());
+        }
+
+        /**
+         * Refactored mod.
+         * 
+         * @return  the module.
+         */
+        public OPT_Complex mod() {
+            return new OPT_Complex(super.abs(), 0.0);
+        }
+        
     }
 	
 	
 	/** The stack in which values are stored. */
 	private Advanced_Stack<OPT_Complex> stack;
+        /** The map containing the variables. */
+	private TreeMap<String, OPT_Complex> vars;
+        /** The map cotaining custom functions. */
+        private TreeMap<String, String> funcs;
+        /** Stack string property. */
+        private SimpleStringProperty stack_p;
+        /** Variables string property. */
+        private SimpleStringProperty vars_p;
+        /** Functions string property. */
+        private SimpleStringProperty funcs_p;
 
 	
 	/**
@@ -148,8 +277,13 @@ public class Calculator {
 	 * 
 	 */
 	public Calculator() {
-		super();
-		this.stack = new Advanced_Stack<>();
+            super();
+            this.stack = new Advanced_Stack<>();
+            this.vars = new TreeMap<>();
+            this.funcs = new TreeMap<>();
+            stack_p = new SimpleStringProperty("");
+            vars_p = new SimpleStringProperty("");
+            funcs_p = new SimpleStringProperty("");
 	}
 	
 	/**
@@ -158,60 +292,108 @@ public class Calculator {
 	 * @param c	the operand.
 	 */
 	public void push_num(OPT_Complex c) {
-		stack.stack_push(c);
+            stack.stack_push(c);
+            this.updateStackProperty();
 	}
 	
 	/**
-	 * Sums all the elements in the stack and updates it.
+	 * Sums the top 2 elements in the stack and updates it.
 	 * 
 	 */
 	public void sum() {
-		ArrayList<OPT_Complex> list = stack.stack_collect();
-		OPT_Complex tmp = list.get(0);
-		for(int i = 1; i < list.size(); i++)
-			tmp = tmp.add(list.get(i));
-                
-		stack.stack_clear();
-		stack.stack_push(tmp);
+            OPT_Complex t1 = stack.stack_drop();
+            Complex t2 = (Complex) stack.stack_drop();
+            stack.stack_push(t1.add(t2));
+            this.updateStackProperty();
+	}
+        
+        /**
+	 * Sums all the elements in the stack and updates it.
+	 * 
+	 */
+	public void all_sum() {
+            ArrayList<OPT_Complex> list = stack.stack_collect();
+            OPT_Complex tmp = list.get(0);
+            for(int i = 1; i < list.size(); i++)
+                tmp = tmp.add(list.get(i));
+            stack.stack_clear();
+            stack.stack_push(tmp);
+            this.updateStackProperty();
 	}
 	
 	/**
-	 * Subtracts all the elements in the stack and updates it.
+	 * Subtracts the top 2 elements in the stack and updates it.
 	 * 
 	 */
 	public void subtract() {
-		ArrayList<OPT_Complex> list = stack.stack_collect();
-		OPT_Complex tmp = list.get(0);
-		for(int i = 1; i < list.size(); i++)
-			tmp = tmp.subtract(list.get(i));
-		stack.stack_clear();
-		stack.stack_push(tmp);
+            OPT_Complex t1 = stack.stack_drop();
+            Complex t2 = (Complex) stack.stack_drop();
+            stack.stack_push(t1.subtract(t2));
+            this.updateStackProperty();
+	}
+        
+        /**
+	 * Subtracts all the elements in the stack and updates it.
+	 * 
+	 */
+	public void all_subtract() {
+            ArrayList<OPT_Complex> list = stack.stack_collect();
+            OPT_Complex tmp = list.get(list.size() - 1);
+            for(int i = list.size() - 2; i >= 0; i--)
+                tmp = tmp.subtract(list.get(i));
+            stack.stack_clear();
+            stack.stack_push(tmp);
+            this.updateStackProperty();
 	}
 	
 	/**
-	 * Multiplies all the elements in the stack and updates it.
+	 * Multiplies the top 2 elements in the stack and updates it.
 	 * 
 	 */
 	public void multiply() {
-		ArrayList<OPT_Complex> list = stack.stack_collect();
-		OPT_Complex tmp = list.get(0);
-		for(int i = 1; i < list.size(); i++)
-			tmp = tmp.multiply(list.get(i));
-		stack.stack_clear();
-		stack.stack_push(tmp);
+            OPT_Complex t1 = stack.stack_drop();
+            Complex t2 = (Complex) stack.stack_drop();
+            stack.stack_push(t1.multiply(t2));
+            this.updateStackProperty();
+	}
+        
+        /**
+	 * Multiplies all the elements in the stack and updates it.
+	 * 
+	 */
+	public void all_multiply() {
+            ArrayList<OPT_Complex> list = stack.stack_collect();
+            OPT_Complex tmp = list.get(0);
+            for(int i = 1; i < list.size(); i++)
+		tmp = tmp.multiply(list.get(i));
+            stack.stack_clear();
+            stack.stack_push(tmp);
+            this.updateStackProperty();
 	}
 	
 	/**
-	 * Divides all the elements in the stack and updates it.
+	 * Divides the top 2 elements in the stack and updates it.
 	 * 
 	 */
 	public void divide() {
-		ArrayList<OPT_Complex> list = stack.stack_collect();
-		OPT_Complex tmp = list.get(0);
-		for(int i = 1; i < list.size(); i++)
-			tmp = tmp.divide(list.get(i));
-		stack.stack_clear();
-		stack.stack_push(tmp);
+            OPT_Complex t1 = stack.stack_drop();
+            Complex t2 = (Complex) stack.stack_drop();
+            stack.stack_push(t1.divide(t2));
+            this.updateStackProperty();
+	}
+        
+        /**
+	 * Divides all the elements in the stack and updates it.
+	 * 
+	 */
+	public void all_divide() {
+            ArrayList<OPT_Complex> list = stack.stack_collect();
+            OPT_Complex tmp = list.get(list.size() - 1);
+            for(int i = list.size() - 2; i >= 0; i--)
+                tmp = tmp.divide(list.get(i));
+            stack.stack_clear();
+            stack.stack_push(tmp);
+            this.updateStackProperty();
 	}
 	
 	/**
@@ -220,36 +402,30 @@ public class Calculator {
 	 * 
 	 */
 	public void power() {
-		ArrayList<OPT_Complex> list = stack.stack_collect();
-		OPT_Complex tmp = list.get(0);
-		for(int i = 1; i < list.size(); i++)
-			tmp = tmp.pow(list.get(i));
-		stack.stack_clear();
-		stack.stack_push(tmp);
+            OPT_Complex t1 = stack.stack_drop();
+            Complex t2 = (Complex) stack.stack_drop();
+            stack.stack_push(t1.pow(t2));
+            this.updateStackProperty();
 	}
 	
 	/**
-	 * Calculates the square root for all the elements in the stack.
+	 * Calculates the square root for the top element in the stack.
 	 * 
 	 */
 	public void sqrt() {
-		ArrayList<OPT_Complex> list = stack.stack_collect();
-		stack.stack_clear();
-		for(OPT_Complex t : list) {
-			stack.stack_push(t.sqrt());
-		}
+            OPT_Complex t1 = stack.stack_drop();
+            stack.stack_push(t1.sqrt());
+            this.updateStackProperty();
 	}
 	
 	/**
-	 * Negates the sign of all the elements in the stack.
+	 * Negates the sign of the top element in the stack.
 	 * 
 	 */
 	public void negate() {
-		ArrayList<OPT_Complex> list = stack.stack_collect();
-		stack.stack_clear();
-		for(OPT_Complex t : list) {
-			stack.stack_push(t.negate());
-		}
+            OPT_Complex t1 = stack.stack_drop();
+            stack.stack_push(t1.negate());
+            this.updateStackProperty();
 	}
 	
 	/**
@@ -257,7 +433,8 @@ public class Calculator {
 	 * 
 	 */
 	public void clear() {
-		stack.stack_clear();
+            stack.stack_clear();
+            this.updateStackProperty();
 	}
 	
 	/**
@@ -265,7 +442,8 @@ public class Calculator {
 	 * 
 	 */
 	public void drop() {
-		stack.stack_drop();
+            stack.stack_drop();
+            this.updateStackProperty();
 	}
 	
 	/**
@@ -273,7 +451,8 @@ public class Calculator {
 	 * 
 	 */
 	public void dup() {
-		stack.stack_dup();
+            stack.stack_dup();
+            this.updateStackProperty();
 	}
 	
 	/**
@@ -281,7 +460,8 @@ public class Calculator {
 	 * 
 	 */
 	public void swap() {
-		stack.stack_swap();
+            stack.stack_swap();
+            this.updateStackProperty();
 	}
 	
 	/**
@@ -290,7 +470,8 @@ public class Calculator {
 	 * 
 	 */
 	public void over() {
-		stack.stack_over();
+            stack.stack_over();
+            this.updateStackProperty();
 	}
         
         /**
@@ -308,7 +489,51 @@ public class Calculator {
          * @param s the number.
          * @throws ParseException when not correctly parsed.
          */
-        public void parse_num(String s) throws ParseException {
+        public void parse_num(String s) throws NumberFormatException {
+            s = s.replace(" ", "").replace("\t", "");
+            boolean firstPositive = true;
+            boolean secondPositive = true;
+            if (s.charAt(0) == '-')
+                firstPositive = false;
+            if (s.substring(1).contains("-"))
+                secondPositive = false;
+            String[] split = s.split("[+-]");
+            int l = split.length;
+            if (split[0].equals("")) {
+                split[0] = split[1];
+                if(split.length > 2)
+                    split[1] = split[2];
+                else
+                    --l;
+            }
+            double realPart = 0;
+            double imgPart = 0;
+            boolean t = split[0].contains("i") || split[0].contains("j");
+            if (t)
+                imgPart = Double.parseDouble((firstPositive ? "+" : "-") + split[0].substring(0,split[0].length() - 1));
+            else
+                realPart = Double.parseDouble((firstPositive ? "+" : "-") + split[0]);
+            if (l > 1) {
+                t = split[1].contains("i") || split[1].contains("j");
+                if (t)
+                    imgPart = Double.parseDouble((secondPositive ? "+" : "-") + split[1].substring(0,split[1].length() - 1));
+                else
+                    realPart = Double.parseDouble((secondPositive ? "+" : "-") + split[1]);
+            }
+            this.push_num(new OPT_Complex(realPart,imgPart));
+        }
+        
+        /**
+         * Parses a sequence of strings, all inside parenthesis;
+         * Stack property is update cumulatively at last.
+         * 
+         * @param i the array of strings;
+         */
+        public void clear_parse_seq(String[] i) {
+            stack.stack_clear();
+            for (String s : i){
+                s = s.replace("(", "").replace(")", "");
+                s = s.replace(" ", "").replace("\t", "");
                 boolean firstPositive = true;
                 boolean secondPositive = true;
                 if (s.charAt(0) == '-')
@@ -338,7 +563,99 @@ public class Calculator {
                     else
                         realPart = Double.parseDouble((secondPositive ? "+" : "-") + split[1]);
                 }
-                this.push_num(new OPT_Complex(realPart,imgPart));
+                stack.stack_push(new OPT_Complex(realPart,imgPart));
+            }
+            this.updateStackProperty();
         }
         
+        /**
+	 * Adds the complex number at the stack's top 
+	 * to the map at the specified key.
+	 * 
+	 * @param key	the variable.
+	 */
+	public void add_var(String key) {
+            vars.put(key, stack.stack_drop());
+            this.updateStackProperty();
+            this.updateVarsProperty();
+	}
+	
+	/**
+	 * Adds the complex number stored in the variable
+	 * to the stack.
+	 * 
+	 * @param key	the variable.
+	 * @throws NullPointerException	when there is no entry corresponding to the key.
+	 */
+	public void put_var(String key) throws NullPointerException {
+            if (!vars.containsKey(key))
+                throw new NullPointerException();
+            stack.stack_push(vars.get(key));
+            this.updateStackProperty();
+            this.updateVarsProperty();
+	}
+	
+	/**
+	 * Sums the number at the stack's top to the variable
+	 * specified and updates the variable.
+	 * 
+	 * @param key	the variable.
+	 * @throws NullPointerException	when there is no entry corresponding to the key.
+	 */
+	public void sum_var(String key) throws NullPointerException {
+            if (!vars.containsKey(key))
+                throw new NullPointerException();
+            vars.put(key, (OPT_Complex) vars.get(key).add(stack.stack_drop()));
+            this.updateStackProperty();
+            this.updateVarsProperty();
+	}
+	
+	/**
+	 * Subtracts the number at the stack's top to the variable
+	 * specified and updates the variable.
+	 * 
+	 * @param key	the variable.
+	 * @throws NullPointerException	when there is no entry corresponding to the key.
+	 */
+	public void sub_var(String key) throws NullPointerException {
+            if (!vars.containsKey(key))
+                throw new NullPointerException();
+            vars.put(key, (OPT_Complex) vars.get(key).subtract(stack.stack_drop()));
+            this.updateStackProperty();
+            this.updateVarsProperty();
+	}
+        
+        /**
+         * Retrieves the variables map.
+         * 
+         * @return  the variables map.
+         */
+        public TreeMap<String, OPT_Complex> getVars() {
+            return this.vars;
+        }
+        
+        /**
+         * Parses operations between stack and variables.
+         * 
+         * @param s the variable command.
+         * @return  if the variable has been correctly identified.
+         */
+        public boolean parse_var(String s) {
+            if (s.length() == 2 && Character.isLowerCase(s.charAt(1))
+                && (s.charAt(0) == '>' || s.charAt(0) == '<' || s.charAt(0) == '+' || s.charAt(0) == '-')) {
+                String v = s.substring(1);
+                if (s.charAt(0) == '>')
+                    this.add_var(v);
+                if (s.charAt(0) == '<')
+                    this.put_var(v);
+                if (s.charAt(0) == '+')
+                    this.sum_var(v);
+                if (s.charAt(0) == '-')
+                    this.sub_var(v);
+                return true;
+            }
+            else
+                return false;
+        }
+    
 }
